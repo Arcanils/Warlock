dc45ffa72c82ba9ecb6403c1d75e872aa247ce77
Petiot Thibaut <petiotthib@gmail.com>
2018-06-25 14:07:42 +0200

    Dll missing, popcorn fx, prefab to spawn on shoot

---
+++ SRC/Assets/Script/BulletBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+
+	public float Speed;
+	public float Duration;
+	public float ExplosionRadius;
+	public float ExplosionForce;
+
+	private void Awake()
+	{
+		
+	}
+
+	private void OnExplosion()
+	{
+		//Spawn Explosion
+	}
+}
